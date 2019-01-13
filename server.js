723ba80ae8b2c120f78367234e57467faa07d889
Shashank <shashankgpt270@gmail.com>
2019-01-13 08:16:35 +0530

    chat bot phase 1 completed

--- server.js
+++ server.js
@@ -3,28 +3,51 @@ var bodyParser=require('body-parser');
 var app=express();
 var http=require('http').Server(app);
 var io =require('socket.io')(http);
+var mongoose=require('mongoose');
+
+var dbUrl='mongodb://localhost:27017/nodebot';
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+var Message=mongoose.model('Message',{
+    name:String,
+    message:String
+})
 var messages=[{name:'Tim',message:'Hi'},
             {name:'Jane',message:'Hello'}
 ]
 
 
 app.get('/messages',(req,res)=>{
-res.send(messages);
+    Message.find({},(err,messages)=>{
+        res.send(messages);
+    })
+//res.send(messages);
 });
 
 app.post('/messages',(req,res)=>{
-    messages.push(req.body);
-    io.emit('message',req.body);
-    res.sendStatus(200);
+    var message=new Message(req.body);
+
+    message.save((err)=>{
+        if(err)
+            sendStatus(500);
+
+            messages.push(req.body);
+            io.emit('message',req.body);
+            res.sendStatus(200);
+        });
+    
     });
 
     io.on('connection',(socket)=>{
         console.log('a user connected');
     });
+mongoose.connect(dbUrl,{useNewUrlParser:true},(err)=>{
+console.log('mongo db connection',err);
+
+});
 var server=http.listen(3000,()=>{
     console.log('server is listening on port',server.address().port);
 });
